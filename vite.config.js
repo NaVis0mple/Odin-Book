a5cea2c5ea52c68eb3480e35f7133819a6d46f47
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-12-05 15:38:58 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Odin-Book/'
+  base: '/Odin-Book/',
+  envDir: './'
 })
